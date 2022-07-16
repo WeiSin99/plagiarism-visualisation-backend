0db200d46496f0c29b3d92c044b2ac7cc7de54ed
WeiSin99 <looweisin008@gmail.com>
2022-07-16 18:08:03 +0100

    Populate source document

---
+++ plagiarism_visualisation_backend/documents/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class Document(models.Model):
+    DOCUMENT_TYPE = [
+        ("source", "source"),
+        ("suspicious", "suspicious"),
+    ]
+
+    title = models.CharField(max_length=1000)
+    slug = models.SlugField(max_length=1000, unique=True)
+    type = models.CharField(max_length=15, choices=DOCUMENT_TYPE)
+    language = models.CharField(max_length=15)
+    raw_text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Sentence(models.Model):
+    document = models.ForeignKey(
+        Document, on_delete=models.CASCADE, related_name="sentences"
+    )
+    raw_text = models.TextField()
+    number = models.IntegerField()
+
+    def __str__(self):
+        return self.raw_text
+
+    class Meta:
+        ordering = ["document", "number"]
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, unique=True)
+    document = models.ManyToManyField(Document, related_name="authors", blank=True)
+
+    def __str__(self):
+        return self.name
