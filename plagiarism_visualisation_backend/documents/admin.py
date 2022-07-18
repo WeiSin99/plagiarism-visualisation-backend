d0b3afc82eb2a9b85e60456528db9fa918b4e1ca
Wei Sin Loo <45273464+WeiSin99@users.noreply.github.com>
2022-07-18 01:43:54 +0100

    Populate database (#2)
    * populate suspicious documents
    
    * update
    
    * update

--- plagiarism_visualisation_backend/documents/admin.py
+++ plagiarism_visualisation_backend/documents/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Document, Sentence, Author
+from .models import Document, Sentence, Author, SuspiciousDocument, SuspiciousSentence
 
 # Register your models here.
 admin.site.register(Document)
 admin.site.register(Sentence)
 admin.site.register(Author)
+admin.site.register(SuspiciousDocument)
+admin.site.register(SuspiciousSentence)
--- plagiarism_visualisation_backend/documents/models.py
+++ plagiarism_visualisation_backend/documents/models.py
@@ -26,6 +26,7 @@ class Sentence(models.Model):
         Document, on_delete=models.CASCADE, related_name="sentences"
     )
     raw_text = models.TextField()
+    preprocessed_text = models.TextField(null=True, blank=True)
     number = models.IntegerField()
 
     def __str__(self):
@@ -42,3 +43,32 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class SuspiciousDocument(models.Model):
+    title = models.CharField(max_length=1000)
+    slug = models.SlugField(max_length=1000, unique=True)
+    doc_num = models.IntegerField(unique=True)
+    language = models.CharField(max_length=15)
+    raw_text = models.TextField()
+
+    class Meta:
+        ordering = ["-doc_num"]
+
+    def __str__(self):
+        return self.title
+
+
+class SuspiciousSentence(models.Model):
+    document = models.ForeignKey(
+        SuspiciousDocument, on_delete=models.CASCADE, related_name="sentences"
+    )
+    raw_text = models.TextField()
+    preprocessed_text = models.TextField(null=True, blank=True)
+    number = models.IntegerField()
+
+    def __str__(self):
+        return self.raw_text
+
+    class Meta:
+        ordering = ["document", "number"]
