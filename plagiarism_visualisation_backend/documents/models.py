45ff4f198d3fd419b5ee1dc3f66274e074a9493b
WeiSin99 <looweisin008@gmail.com>
2022-08-02 17:56:58 +0100

    Populate document authors

--- plagiarism_visualisation_backend/documents/models.py
+++ plagiarism_visualisation_backend/documents/models.py
@@ -2,17 +2,12 @@ from django.db import models
 
 
 class Document(models.Model):
-    DOCUMENT_TYPE = [
-        ("source", "source"),
-        ("suspicious", "suspicious"),
-    ]
-
     title = models.CharField(max_length=1000)
     slug = models.SlugField(max_length=1000, unique=True)
     doc_num = models.IntegerField(unique=True)
-    type = models.CharField(max_length=15, choices=DOCUMENT_TYPE)
     language = models.CharField(max_length=15)
     raw_text = models.TextField()
+    authors = models.TextField(null=True, blank=True)
 
     def serialize(self):
         return {
@@ -49,21 +44,13 @@ class Sentence(models.Model):
         ordering = ["document", "number"]
 
 
-class Author(models.Model):
-    name = models.CharField(max_length=150)
-    slug = models.SlugField(max_length=150, unique=True)
-    document = models.ManyToManyField(Document, related_name="authors", blank=True)
-
-    def __str__(self):
-        return self.name
-
-
 class SuspiciousDocument(models.Model):
     title = models.CharField(max_length=1000)
     slug = models.SlugField(max_length=1000, unique=True)
     doc_num = models.IntegerField(unique=True)
     language = models.CharField(max_length=15)
     raw_text = models.TextField()
+    authors = models.TextField(null=True, blank=True)
 
     def serialize(self):
         return {
