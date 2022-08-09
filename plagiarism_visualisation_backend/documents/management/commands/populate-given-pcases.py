424f0a5645ab59039ad25f957fd303a0527f44da
WeiSin99 <looweisin008@gmail.com>
2022-08-09 21:12:54 +0100

    update

--- plagiarism_visualisation_backend/documents/management/commands/populate-given-pcases.py
+++ plagiarism_visualisation_backend/documents/management/commands/populate-given-pcases.py
@@ -169,116 +169,116 @@ class Command(BaseCommand):
     help = "Populate database with given plagiarism cases from dataset."
 
     def handle(self, *args, **options):
-        suspicious_doc_num = 10375
-        case_num = 29
-        plagiarised_parts = get_plagiarised_part(suspicious_doc_num)
-        print(len(plagiarised_parts))
-        plagiarised_part = plagiarised_parts[case_num]
-
-        print("==============suspicious================")
-        print(plagiarised_part[2])
-        sus_target_sents = get_target_sentences(
-            suspicious_doc_num, "suspicious", plagiarised_part[2]
-        )
-        print(sus_target_sents)
-        # print("=====================================")
-        # sus_start = 1055
-        # sus_end = 1055
-        # print(get_sentences(suspicious_doc_num, "suspicious", sus_start, sus_end))
-
-        print("==============source================")
-        print(plagiarised_part[3])
-        source_target_sents = get_target_sentences(
-            plagiarised_part[0], "source", plagiarised_part[3]
-        )
-        print(source_target_sents)
-        # print("=====================================")
-        # source_start = 63
-        # source_end = 63
-        # print(get_sentences(plagiarised_part[0], "source", source_start, source_end))
-
-        # create_given_case(
-        #     sus_doc_num=suspicious_doc_num,
-        #     sus_start=sus_start,
-        #     sus_end=sus_end,
-        #     source_doc_num=plagiarised_part[0],
-        #     source_start=source_start,
-        #     source_end=source_end,
-        #     obfuscation=plagiarised_part[1],
-        #     type=plagiarised_part[4],
+        # suspicious_doc_num = 10375
+        # case_num = 29
+        # plagiarised_parts = get_plagiarised_part(suspicious_doc_num)
+        # print(len(plagiarised_parts))
+        # plagiarised_part = plagiarised_parts[case_num]
+        #
+        # print("==============suspicious================")
+        # print(plagiarised_part[2])
+        # sus_target_sents = get_target_sentences(
+        #     suspicious_doc_num, "suspicious", plagiarised_part[2]
         # )
+        # print(sus_target_sents)
+        # # print("=====================================")
+        # # sus_start = 1055
+        # # sus_end = 1055
+        # # print(get_sentences(suspicious_doc_num, "suspicious", sus_start, sus_end))
+        #
+        # print("==============source================")
+        # print(plagiarised_part[3])
+        # source_target_sents = get_target_sentences(
+        #     plagiarised_part[0], "source", plagiarised_part[3]
+        # )
+        # print(source_target_sents)
+        # # print("=====================================")
+        # # source_start = 63
+        # # source_end = 63
+        # # print(get_sentences(plagiarised_part[0], "source", source_start, source_end))
+        #
+        # # create_given_case(
+        # #     sus_doc_num=suspicious_doc_num,
+        # #     sus_start=sus_start,
+        # #     sus_end=sus_end,
+        # #     source_doc_num=plagiarised_part[0],
+        # #     source_start=source_start,
+        # #     source_end=source_end,
+        # #     obfuscation=plagiarised_part[1],
+        # #     type=plagiarised_part[4],
+        # # )
 
-        # curpath = os.path.dirname(__file__)
-        # with open(os.path.join(curpath, "./logs.txt"), "a") as log_file:
-        #     for suspicious_doc_num in range(1, 11094):
-        #         plagiarised_parts = get_plagiarised_part(suspicious_doc_num)
-        #         # print(f"file {suspicious_doc_num}")
-        #         # print(f"number of plagiarised parts: {len(plagiarised_parts)}")
-        #
-        #         for idx, plagiarised_part in enumerate(plagiarised_parts):
-        #             # print(f"Case: {idx + 1}")
-        #             sus_target_sents = get_target_sentences(
-        #                 suspicious_doc_num, "suspicious", plagiarised_part[2]
-        #             )
-        #             if len(sus_target_sents) <= 0:
-        #                 print(f"suspicious document: {suspicious_doc_num}")
-        #                 print(f"case: {idx}")
-        #                 print("")
-        #                 continue
-        #             sus_start, sus_end = longest_consecutive_sentences(sus_target_sents)
-        #             sus_sent_num = sus_end - sus_start + 1
-        #
-        #             source_target_sents = get_target_sentences(
-        #                 plagiarised_part[0], "source", plagiarised_part[3]
-        #             )
-        #             if len(source_target_sents) <= 0:
-        #                 print(f"suspicious document: {suspicious_doc_num}")
-        #                 print(f"case: {idx}")
-        #                 print("")
-        #                 continue
-        #             source_start, source_end = longest_consecutive_sentences(
-        #                 source_target_sents
-        #             )
-        #             source_sent_num = source_end - source_start + 1
-        #
-        #             if (
-        #                 sus_sent_num <= 2
-        #                 or source_sent_num <= 2
-        #                 or abs(source_sent_num - sus_sent_num) > 10
-        #             ):
-        #                 log_file.write(f"suspicious document: {suspicious_doc_num}\n")
-        #                 log_file.write(f"case: {idx}\n")
-        #                 log_file.write("===============suspicious===============\n")
-        #                 log_file.write(plagiarised_part[2])
-        #                 log_file.write("\n==============================\n")
-        #                 log_file.write(
-        #                     get_sentences(
-        #                         suspicious_doc_num,
-        #                         "suspicious",
-        #                         sus_start,
-        #                         sus_end,
-        #                     )
-        #                 )
-        #                 log_file.write("\n===============source===============\n")
-        #                 log_file.write(plagiarised_part[3])
-        #                 log_file.write("\n================================\n")
-        #                 log_file.write(
-        #                     get_sentences(
-        #                         plagiarised_part[0],
-        #                         "source",
-        #                         source_start,
-        #                         source_end,
-        #                     )
-        #                 )
-        #                 log_file.write("\n\n")
-        #
-        #             create_given_case(
-        #                 sus_doc_num=suspicious_doc_num,
-        #                 sus_start=sus_start,
-        #                 sus_end=sus_end,
-        #                 source_doc_num=plagiarised_part[0],
-        #                 source_start=source_start,
-        #                 source_end=source_end,
-        #                 obfuscation=plagiarised_part[1],
-        #                 type=plagiarised_part[4],
-        #             )
+        curpath = os.path.dirname(__file__)
+        with open(os.path.join(curpath, "./logs.txt"), "a") as log_file:
+            for suspicious_doc_num in range(1, 11094):
+                plagiarised_parts = get_plagiarised_part(suspicious_doc_num)
+                # print(f"file {suspicious_doc_num}")
+                # print(f"number of plagiarised parts: {len(plagiarised_parts)}")
+
+                for idx, plagiarised_part in enumerate(plagiarised_parts):
+                    # print(f"Case: {idx + 1}")
+                    sus_target_sents = get_target_sentences(
+                        suspicious_doc_num, "suspicious", plagiarised_part[2]
+                    )
+                    if len(sus_target_sents) <= 0:
+                        print(f"suspicious document: {suspicious_doc_num}")
+                        print(f"case: {idx}")
+                        print("")
+                        continue
+                    sus_start, sus_end = longest_consecutive_sentences(sus_target_sents)
+                    sus_sent_num = sus_end - sus_start + 1
+
+                    source_target_sents = get_target_sentences(
+                        plagiarised_part[0], "source", plagiarised_part[3]
+                    )
+                    if len(source_target_sents) <= 0:
+                        print(f"suspicious document: {suspicious_doc_num}")
+                        print(f"case: {idx}")
+                        print("")
+                        continue
+                    source_start, source_end = longest_consecutive_sentences(
+                        source_target_sents
+                    )
+                    source_sent_num = source_end - source_start + 1
+
+                    if (
+                        sus_sent_num <= 2
+                        or source_sent_num <= 2
+                        or abs(source_sent_num - sus_sent_num) > 10
+                    ):
+                        log_file.write(f"suspicious document: {suspicious_doc_num}\n")
+                        log_file.write(f"case: {idx}\n")
+                        log_file.write("===============suspicious===============\n")
+                        log_file.write(plagiarised_part[2])
+                        log_file.write("\n==============================\n")
+                        log_file.write(
+                            get_sentences(
+                                suspicious_doc_num,
+                                "suspicious",
+                                sus_start,
+                                sus_end,
+                            )
+                        )
+                        log_file.write("\n===============source===============\n")
+                        log_file.write(plagiarised_part[3])
+                        log_file.write("\n================================\n")
+                        log_file.write(
+                            get_sentences(
+                                plagiarised_part[0],
+                                "source",
+                                source_start,
+                                source_end,
+                            )
+                        )
+                        log_file.write("\n\n")
+
+                    create_given_case(
+                        sus_doc_num=suspicious_doc_num,
+                        sus_start=sus_start,
+                        sus_end=sus_end,
+                        source_doc_num=plagiarised_part[0],
+                        source_start=source_start,
+                        source_end=source_end,
+                        obfuscation=plagiarised_part[1],
+                        type=plagiarised_part[4],
+                    )
